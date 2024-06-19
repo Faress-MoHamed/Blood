b8614da68e154d84bc554a905f408a0d1f8f6178
FARES MOHAMED <119041871+Faress-MoHamed@users.noreply.github.com>
2024-06-19 23:54:00 +0300

    11:53

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { AuthProvider } from "./Context/AuthProvider";
 import RequireAuth from "./Components/RequireAuth";
 import ServicesUser from "./Pages/ServicesUser";
 import Profile from "./Pages/Profile";
-import ServicesBloodBank from './Pages/ServicesBloodBank';
+import ServicesBloodBank from "./Pages/ServicesBloodBank";
 
 function App() {
 	return (
@@ -20,12 +20,12 @@ function App() {
 					<Route element={<Layout />} path="/">
 						<Route element={<Home />} index={true} path="/" />
 						<Route element={<RequireAuth />} path="/">
-							{
-								JSON.parse(localStorage.getItem("user"))?.role!=="bloodBank"?
-									<Route element={<ServicesUser />} path="/Services" /> :
-									<Route element={<ServicesBloodBank />} path="/Services" /> 
-									
-							}
+							{JSON.parse(localStorage.getItem("user") || {})?.role !==
+							"bloodBank" ? (
+								<Route element={<ServicesUser />} path="/Services" />
+							) : (
+								<Route element={<ServicesBloodBank />} path="/Services" />
+							)}
 						</Route>
 						<Route element={<RequireAuth />} path="/">
 							<Route element={<Profile />} path="/Profile" />
